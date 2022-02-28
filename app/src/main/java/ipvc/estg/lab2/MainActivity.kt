d3b6998932ff711fc5c74ce9fa9f6c9bd88e613b
D3z010 <franciscorodrigues@ipvc.pt>
2022-02-28 12:28:10 +0000

    Initial commit

---
+++ app/src/main/java/ipvc/estg/lab2/MainActivity.kt
@@ -0,0 +1,36 @@
+package ipvc.estg.lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("tag","metodo onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("tag","metodo onStop")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("tag","metodo onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("tag","metodo onDestroy")
+    }
+
+// teste no1
+
+
+}
